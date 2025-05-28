4af75cd3492b009d1c830a0e862e20305645db1d
Dev Prashant <dev.prashant1901@gmail.com>
2025-05-28 19:40:21 +0530

    file submit using multer, user detail extraction

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,18 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler( async (req, res) => {
-    return res.status(200).json({
-        message:"Registered",
-    })
-    })
+    //get user details from frontend
+    //validation - non empty data
+    // user already exists (username/email se)
+    //check for images,avatars
+    //upload to cloudinary,avatar
+    //create user object - create entry in DB
+    //remove pass and refresh token from response
+    //check for user creation(hua ya nhi)
+    //return res else error
+
+    const {fullName, email,username,password} = req.body;
+    console.log(email,password)
+})
 
 export {registerUser};
